3bffa6d3fa9034db556d16670833b67c5e32e0bc
AdrielRA <36385005+AdrielRA@users.noreply.github.com>
2021-07-20 10:29:50 -0300

    :rocket: #0009 Finaliza navbar

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,52 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Container, Footer, Header } from "./styles";
+import React from "react";
+import { Container, Footer } from "./styles";
+import { Header } from "../../components";
 import { Home } from "../Home";
 import { Biography } from "../Biography";
 import { Procedures } from "../Procedures";
 import { Evaluation } from "../Evaluation";
 import { Courses } from "../Courses";
 import footerBg from "../../assets/svg/footer.svg";
-import logo from "../../assets/svg/icon-white.svg";
 
 export function LandingPage() {
-  const [headerBgOpacity, setHeaderBgOpacity] = useState(0);
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScrolled);
-    return () => window.removeEventListener("scroll", onScrolled);
-  }, []);
-
-  const onScrolled = (e) => {
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop;
-
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-
-    const scrolled = winScroll / 250;
-
-    setHeaderBgOpacity(scrolled > 1 ? 1 : scrolled);
-  };
-
   return (
     <Container>
-      <Header bgOpacity={headerBgOpacity}>
-        <img src={logo} alt="Brunna Gomes Logo" onClick />
-        <nav>
-          <ul>
-            <li>
-              <a href="#procedures">Procedimentos</a>
-            </li>
-            <li>
-              <a href="#evaluation">Consulta/Avaliação</a>
-            </li>
-            <li>
-              <a href="#courses">Cursos ministrados</a>
-            </li>
-          </ul>
-        </nav>
-      </Header>
+      <Header />
       <Home />
       <Biography />
       <Procedures />
