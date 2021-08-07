bc6fec89a4af539538ac447929773e2aa0912e3d
Lucas Abreu <lucas.abreu@legalcontrol.com.br>
2021-08-07 15:20:30 -0300

    :adhesive_bandage: #17 Tirando configurações do gitpages

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import { LandingPage } from './pages/LandingPage';
+import { LandingPage } from "./pages/LandingPage";
 
 const Routes = () => (
-    <BrowserRouter basename="/SiteBrunnaGomes">
-        <Switch>
-            <Route path="/" component={LandingPage} />
-        </Switch>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
